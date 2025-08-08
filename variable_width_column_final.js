df2534da91e174ac9ceeca9ec05f73998a1fc6dc
dsxavier-vinicius <78446138+dsxavier-vinicius@users.noreply.github.com>
2025-08-08 17:07:55 +0100

    Update variable_width_column_final.js

--- variable_width_column_final.js
+++ variable_width_column_final.js
@@ -1,127 +1,113 @@
 looker.plugins.visualizations.add({
   id: "variable_width_column_final",
   label: "Variable Width Column High",
+
   options: {
     bar_color: {
       type: "string",
       label: "Bar Color",
-      display: "color",
-      default: "#FF9999"
+      default: "#3399ff",
+      display: "color"
     },
     color_by_point: {
       type: "boolean",
       label: "Color by Point",
-      default: false
-    },
-    palette: {
-      type: "string",
-      label: "Color Palette",
-      display: "select",
-      default: "default",
-      values: [
-        { "Looker Default": "default" },
-        { "Gray": "gray" },
-        { "Blue": "blue" },
-        { "Purple": "purple" },
-        { "Orange": "orange" },
-        { "Green": "green" },
-        { "Pink": "pink" },
-        { "Teal": "teal" }
-      ]
+      default: true
     }
   },
 
-  create: function (element, config) {
-    element.innerHTML = "<div id='chart'></div>";
+  create: function(element, config) {
+    element.innerHTML = '<div id="chart-container" style="width:100%; height:500px;"></div>';
+
+    function loadScript(src, callback) {
+      const script = document.createElement('script');
+      script.src = src;
+      script.onload = callback;
+      document.head.appendChild(script);
+    }
+
+    if (!window.Highcharts || !window.Highcharts.chart) {
+      loadScript("https://code.highcharts.com/highcharts.js", () => {
+        this.highchartsLoaded = true;
+      });
+    } else {
+      this.highchartsLoaded = true;
+    }
   },
 
-  updateAsync: function (data, element, config, queryResponse, details, done) {
-    try {
-      if (queryResponse.fields.dimensions.length < 1 || queryResponse.fields.measures.length < 2) {
-        element.innerHTML = "This chart requires 1 dimension and 2 measures.";
-        return done();
+  updateAsync: function(data, element, config, queryResponse, details, done) {
+    if (!this.highchartsLoaded) {
+      setTimeout(() => this.updateAsync(data, element, config, queryResponse, details, done), 100);
+      return;
+    }
+
+    const dimension = queryResponse.fields.dimensions[0];
+    const measure = queryResponse.fields.measures[0];
+    const volumeField = queryResponse.fields.measures[1];
+
+    const rawData = data.map(row => ({
+      name: row[dimension.name].value,
+      y: row[measure.name].value,
+      z: volumeField ? row[volumeField.name].value : 100,
+      originalValues: {
+        measure: row[measure.name].value,
+        volume: volumeField ? row[volumeField.name].value : null
       }
+    }));
 
-      const dimension = queryResponse.fields.dimensions[0].name;
-      const measureValue = queryResponse.fields.measures[0].name;
-      const measureWidth = queryResponse.fields.measures[1].name;
+    const maxZ = Math.max(...rawData.map(d => d.z));
+    const maxPointWidth = 100;
 
-      const labels = [];
-      const values = [];
-      const widths = [];
+    const dataWithWidth = rawData.map(d => ({
+      name: d.name,
+      y: d.y,
+      pointWidth: (d.z / maxZ) * maxPointWidth,
+      originalValues: d.originalValues
+    }));
 
-      data.forEach(row => {
-        labels.push(row[dimension].value);
-        values.push(Number(row[measureValue].value));
-        widths.push(Number(row[measureWidth].value));
-      });
-
-      const barColor = config.bar_color || "#FF9999";
-      const useColorByPoint = config.color_by_point;
-
-      let colors = [];
-      if (useColorByPoint) {
-        const palette = LookerCharts.Utils.getPalette(config.palette || "default");
-        colors = palette.colors.slice(0, values.length);
-      }
-
-      const seriesData = labels.map((label, i) => ({
-        name: label,
-        y: values[i],
-        pointWidth: widths[i],
-        color: useColorByPoint ? colors[i] : barColor
-      }));
-
-      Highcharts.chart("chart", {
-        chart: {
-          type: "column"
-        },
-        title: {
-          text: null
-        },
-        xAxis: {
-          categories: labels,
-          labels: {
-            rotation: -45
-          }
-        },
-        yAxis: {
-          title: {
-            text: queryResponse.fields.measures[0].label
-          }
-        },
-        legend: {
-          enabled: true
-        },
-        series: [{
-          name: queryResponse.fields.measures[0].label,
-          data: seriesData,
-          showInLegend: true
-        }],
-        tooltip: {
-          pointFormat: '{series.name}: <b>{point.y:,.0f}</b>'
-        },
-        plotOptions: {
-          column: {
-            dataLabels: {
-              enabled: true,
-              formatter: function () {
-                return Highcharts.numberFormat(this.y, 0);
-              },
-              style: {
-                fontWeight: "bold",
-                color: "black"
-              }
-            }
+    Highcharts.chart('chart-container', {
+      chart: {
+        type: 'column',
+        animation: { duration: 1000 },
+        spacingTop: 20
+      },
+      title: { text: null },
+      subtitle: { text: null },
+      xAxis: {
+        type: 'category',
+        title: { text: dimension.label },
+        labels: { rotation: -45 }
+      },
+      yAxis: {
+        min: 0,
+        title: { text: measure.label }
+      },
+      tooltip: {
+        formatter: function () {
+          const val1 = Highcharts.numberFormat(this.point.originalValues.measure, 0);
+          const val2 = volumeField ? Highcharts.numberFormat(this.point.originalValues.volume, 0) : null;
+          return `<b>${this.point.name}</b><br>${measure.label}: $${val1}` +
+            (volumeField ? `<br>${volumeField.label}: ${val2}` : '');
+        }
+      },
+      plotOptions: {
+        column: {
+          dataLabels: {
+            enabled: true,
+            format: '${y:,.0f}'
           }
         }
-      });
+      },
+      exporting: { enabled: false },
+      credits: { enabled: false },
+      series: [{
+        name: measure.label,
+        data: dataWithWidth,
+        color: config.bar_color,
+        colorByPoint: config.color_by_point
+      }]
+    });
 
-      done();
-    } catch (error) {
-      element.innerHTML = "Error: " + error.message;
-      console.error(error);
-      done();
-    }
+    done();
   }
 });
